836006805ccb3b2e2b9af2a7a59aa70046d466d9
Valentin Gorbatenko <v.gorbatenko@mail.ru>
2021-06-23 10:08:53 +0300

    Add field validation

--- js/script.js
+++ js/script.js
@@ -1,8 +1,22 @@
+$(document).ready(function(){
+	$('input').change(function() {
+		let el = $(this).parent().parent();
+		if ($(this).val()) {
+			el.addClass('success');
+			el.removeClass('error');
+		} else {
+			el.addClass('error');
+			el.removeClass('success');
+		}
+	});
+});
+
+
 $(document).ready(function() {
 
 	//E-mail Ajax Send
 	$("form").submit(function() { //Change
-		var th = $(this);
+		let th = $(this);
 		$.ajax({
 			type: "POST",
 			url: "../mail.php", //Change
